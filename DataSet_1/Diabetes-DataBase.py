df9719ebf7c491170b42534e3d930dff41772887
TUIRAN QUINTERO NATALIA <ntuiran@cuc.edu.co>
2022-05-18 20:37:01 -0500

    diabetes-dataSet-Kneighbors

--- DataSet_1/Diabetes-DataBase.py
+++ DataSet_1/Diabetes-DataBase.py
@@ -14,9 +14,9 @@ import matplotlib.pyplot as plt
 
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from warnings import simplefilter
 from sklearn.tree import DecisionTreeClassifier
 
 ##Librerias para validación cruzada
@@ -28,9 +28,6 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 
-from sklearn.svm import SVC
-from warnings import simplefilter
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 data = pd.read_csv('diabetes.csv')
@@ -347,6 +344,84 @@ f1 = f1_score(y_test_out, y_pred, average=None).mean()
 
 print(f'f1: {f1}')
 
+
+#-------------------------------------------------------------------------------------------------------
+
+
+
+# K-Nearest neighbors
+
+# Seleccionar un modelo
+
+kneighbors = KNeighborsClassifier()
+
+# Entrenar el modelo
+
+kneighbors.fit(x_train, y_train)
+
+# Metricas
+
+print('*'*50)
+print('K-Nearest neighbors')
+
+# Accuracy de Test de Entrenamiento
+
+print(f'accuracy de Test de Entrenamiento: {kneighbors.score(x_test, y_test)}')
+
+# Accuracy de entrenamiento de Entrenamiento
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {kneighbors.score(x_train, y_train)}')
+
+# Accuracy de validacion
+
+print(f'accuracy de Test de Entrenamiento: {kneighbors.score(x_test_out, y_test_out)}')
+
+# K-Nearest neighbors CON VALIDACIÓN CRUZADA
+
+kfold = KFold(n_splits=10)
+
+acc_scores_train_train = []
+acc_scores_test_train = []
+
+for train, test in kfold.split(x, y): 
+    kneighbors.fit(x[train], y[train]) 
+    scores_train_train = kneighbors.score(x[train], y[train]) 
+    scores_test_train = kneighbors.score(x[test], y[test])
+    acc_scores_train_train.append(scores_train_train)
+    acc_scores_test_train.append(scores_test_train)
+    
+y_pred = svc.predict(x_test_out)
+
+print('*'*50)
+print('K-Nearest neighbors')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'accuracy de Entrenamiento de Entrenamiento: {np.array(acc_scores_train_train).mean()}')
+
+# Accuracy de Test de Entrenamiento
+print(f'accuracy de Test de Entrenamiento: {np.array(acc_scores_test_train).mean()}')
+
+# Accuracy de Validación
+print(f'accuracy de Validación: {kneighbors.score(x_test_out, y_test_out)}')
+
+
+# Matriz de confusión
+print(f'Matriz de confusión: {confusion_matrix(y_test_out, y_pred)}')
+
+matriz_confusion = confusion_matrix(y_test_out, y_pred) 
+plt.figure(figsize = (6, 6)) 
+sns.heatmap(matriz_confusion)
+plt.title("Mariz de confusión")
+
+precision = precision_score(y_test_out, y_pred, average=None).mean()
+print(f'Precisión: {precision}')
+
+recall = recall_score(y_test_out, y_pred, average=None).mean()
+print(f'Re-call: {recall}')
+
+f1 = f1_score(y_test_out, y_pred, average=None).mean()
+
+print(f'f1: {f1}')
 
 
 
