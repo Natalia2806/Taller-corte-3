b81523d5ba6f1d7f5c34c9df160ba1ade9ccd09c
TUIRAN QUINTERO NATALIA <ntuiran@cuc.edu.co>
2022-05-18 19:30:57 -0500

    Dividir la data en train y test

--- DataSet_1/Diabetes-DataBase.py
+++ DataSet_1/Diabetes-DataBase.py
@@ -5,14 +5,28 @@ Created on Thu Apr 28 21:59:47 2022
 @author: Naty
 """
 
+
 import numpy as np
 import pandas as pd
+
+##Librerias para graficar la matriz de confusión
+import matplotlib.pyplot as plt
+
+import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from warnings import simplefilter
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+
+##Librerias para validación cruzada
+from sklearn.model_selection import KFold
+
+##Librerias de metricas para matriz de confusión
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
 data = pd.read_csv('diabetes.csv')
 
