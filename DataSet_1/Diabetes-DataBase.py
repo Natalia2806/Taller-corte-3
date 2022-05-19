3c384181980792565f80a43ee3ee72cd803f8e57
TUIRAN QUINTERO NATALIA <ntuiran@cuc.edu.co>
2022-05-18 19:27:55 -0500

    Normalizar y limpiar la data

--- DataSet_1/Diabetes-DataBase.py
+++ DataSet_1/Diabetes-DataBase.py
@@ -1,7 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Thu Apr 28 21:59:47 2022
 
-This is a temporary script file.
+@author: Naty
 """
-fff
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from warnings import simplefilter
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+data = pd.read_csv('diabetes.csv')
+
+#Normalizar y limpiar la data
+
+data.drop(['Pregnancies', 'BloodPressure', 'SkinThickness'], 
+          axis= 1, inplace = True)
+
+data.Age.replace(np.nan, 33, inplace=True)
+
+ranges = [0, 8, 15, 18, 25, 40, 60, 100]
+
+names = ['1', '2', '3', '4', '5', '6', '7']
+
+data.Age = pd.cut(data.Age, ranges, labels=names)
+
+data.dropna(axis=0,how='any', inplace=True)
