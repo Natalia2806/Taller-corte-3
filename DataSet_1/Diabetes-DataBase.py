34f59fd2583d06f9af80078190c45aa4367f06fe
TUIRAN QUINTERO NATALIA <ntuiran@cuc.edu.co>
2022-05-18 19:58:22 -0500

    regresion logistica

--- DataSet_1/Diabetes-DataBase.py
+++ DataSet_1/Diabetes-DataBase.py
@@ -28,6 +28,11 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 
+from sklearn.svm import SVC
+from warnings import simplefilter
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 data = pd.read_csv('diabetes.csv')
 
 #Normalizar y limpiar la data
@@ -44,3 +49,86 @@ names = ['1', '2', '3', '4', '5', '6', '7']
 data.Age = pd.cut(data.Age, ranges, labels=names)
 
 data.dropna(axis=0,how='any', inplace=True)
+
+
+#Dividir la data en train y test 
+data_train = data[:384]
+data_test = data[384:]
+
+x = np.array(data_train.drop(['Outcome'], axis=1))
+y = np.array(data_test.Outcome)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+x_test_out = np.array(data_test.drop(['Outcome'], axis=1))
+y_test_out = np.array(data_test.Outcome)
+
+## REGRESIÓN LOGÍSTICA
+
+logreg = LogisticRegression(solver='lbfgs', max_iter = 7600)
+
+# Entrenamiento el modelo
+logreg.fit(x_train, y_train)
+
+# MÉTRICAS
+
+print('*'*50)
+print('Regresión Logística')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'accuracy de Entrenamiento de Entrenamiento: {logreg.score(x_train, y_train)}')
+
+# Accuracy de Test de Entrenamiento
+print(f'accuracy de Test de Entrenamiento: {logreg.score(x_test, y_test)}')
+
+# Accuracy de Validación
+print(f'accuracy de Validación: {logreg.score(x_test_out, y_test_out)}')
+
+# REGRESIÓN LOGÍSTICA CON VALIDACIÓN CRUZADA
+
+kfold = KFold(n_splits=10)
+
+acc_scores_train_train = []
+acc_scores_test_train = []
+logreg = LogisticRegression(solver='lbfgs', max_iter = 7600)
+
+for train, test in kfold.split(x, y): 
+    logreg.fit(x[train], y[train]) 
+    scores_train_train = logreg.score(x[train], y[train]) 
+    scores_test_train = logreg.score(x[test], y[test])
+    acc_scores_train_train.append(scores_train_train)
+    acc_scores_test_train.append(scores_test_train)
+    
+y_pred = logreg.predict(x_test_out)
+
+print('*'*50)
+print('Regresión Logística Validación cruzada')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'accuracy de Entrenamiento de Entrenamiento: {np.array(acc_scores_train_train).mean()}')
+
+# Accuracy de Test de Entrenamiento
+print(f'accuracy de Test de Entrenamiento: {np.array(acc_scores_test_train).mean()}')
+
+# Accuracy de Validación
+print(f'accuracy de Validación: {logreg.score(x_test_out, y_test_out)}')
+
+
+# Matriz de confusión
+print(f'Matriz de confusión: {confusion_matrix(y_test_out, y_pred)}')
+
+matriz_confusion = confusion_matrix(y_test_out, y_pred) #Graficar la matriz
+plt.figure(figsize = (6, 6)) #Dimesiones de la matriz
+sns.heatmap(matriz_confusion)
+plt.title("Mariz de confusión")
+
+precision = precision_score(y_test_out, y_pred, average=None).mean()
+print(f'Precisión: {precision}')
+
+recall = recall_score(y_test_out, y_pred, average=None).mean()
+print(f'Re-call: {recall}')
+
+f1_score = f1_score(y_test_out, y_pred, average=None).mean()
+
+print(f'f1: {f1_score}')
+
